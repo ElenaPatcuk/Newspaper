66e858011ec8c5e488ca821504578599d1bcfcc1
Elena Patcuk <1999prsmx@gmail.com>
2023-09-23 21:15:03 +0700

    Updates

---
+++ precache-manifest.ac2fd2996b77083e1aaa2f6805972191.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "710a3001aed432e2f49e",
+    "url": "/Newspaper/static/css/main.912d01ce.chunk.css"
+  },
+  {
+    "revision": "710a3001aed432e2f49e",
+    "url": "/Newspaper/static/js/main.710a3001.chunk.js"
+  },
+  {
+    "revision": "319c9e63ec827bfb8101",
+    "url": "/Newspaper/static/js/1.319c9e63.chunk.js"
+  },
+  {
+    "revision": "b05246ea075054dc8f9b",
+    "url": "/Newspaper/static/js/runtime~main.b05246ea.js"
+  },
+  {
+    "revision": "2fffc8223b6c1cc879b09b17afd2d74d",
+    "url": "/Newspaper/index.html"
+  }
+];
